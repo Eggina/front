796300756e6bd48bdf982da35f1579c7eb9a15b3
Feliciano Franco <eggo.ina@gmail.com>
2020-12-16 09:55:38 -0300

    cleaning

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-SQLALCHEMY_DATABASE_URL = 'sqlite:///data/data.db'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
-
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
